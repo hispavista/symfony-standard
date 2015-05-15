85b80e5ea3bd8f764de37a16274d7f50a8a97732
rubenma <ruben.martin@corp.hispavista.com>
2015-05-15 12:31:04 +0200

    Fix

--- src/Hispavista/WebBundle/Menu/AdminMenuBuilder.php
+++ src/Hispavista/WebBundle/Menu/AdminMenuBuilder.php
@@ -18,7 +18,7 @@ class AdminMenuBuilder extends ContainerAware{
         // create root item
         $menu = $factory->createItem('root');
         // set id for root item, and class for nice twitter bootstrap style
-        $menu->setChildrenAttributes(array('id' => 'main_navigation', 'class' => 'nav'));
+        $menu->setChildrenAttributes(array('id' => 'main_navigation', 'class' => 'nav navbar-nav'));
 
         // add links $menu
         $menu->addChild('Usuarios', array('route'=>'Hispavista_WebBundle_User_list'));
